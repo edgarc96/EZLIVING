1a970fc205baba471dcac72f69eb6e7dadacb28f
Edgar <edgarcabrera33@gmail.com>
2024-02-05 12:42:23 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { NavBar } from "./components/NavBar.js";
+import { Banner } from "./components/Banner.js";
+import { Skills } from "./components/Skills.js";
+import "../src/App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
